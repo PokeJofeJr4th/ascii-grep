3753413fb54f67b2d2d839c6c9649753d04071b7
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2023-11-16 16:43:51 -0500

    use a better index

--- build.rs
+++ build.rs
@@ -10,23 +10,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let entries = content
         .split('\n')
         .flat_map(|row| {
-            println!("{row:?}");
-            let mut idx = row.len();
-            while idx != 0
-                && row
-                    .chars()
-                    .nth(idx - 1)
-                    .as_ref()
-                    .is_some_and(char::is_ascii_hexdigit)
-            {
-                idx -= 1;
-            }
-            let (lhs, rhs) = row.split_at(idx);
-            let char_code = u32::from_str_radix(rhs, 16).ok()?;
+            let mut row_iter = row.split(';');
+            let code = row_iter.next()?;
+            let description = row_iter.next()?;
+            let char_code = u32::from_str_radix(code, 16).ok()?;
             let char = char::from_u32(char_code)?;
             Some(CharEntry {
                 char,
-                description: lhs.to_lowercase(),
+                description: description.to_lowercase(),
             })
         })
         .collect::<Vec<_>>();
